ab232346c69576f0663b45ef83865196a15cd4fd
Amman Rizwan <97797756+AmmanRizwan@users.noreply.github.com>
2025-08-30 13:14:49 +0530

    Refactor Notepad UI: Replace VBox with BorderPane, add menu options and character count feature

--- notepad/src/main/java/gui/project/notepad/Controller.java
+++ notepad/src/main/java/gui/project/notepad/Controller.java
@@ -1,14 +1,24 @@
 package gui.project.notepad;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TextArea;
+import javafx.scene.text.Text;
 
 public class Controller {
     @FXML
-    private Label welcomeText;
+    private TextArea editor;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to Notepad Application!");
+    private MenuItem fileMenuOption;
+
+    @FXML
+    private void countCharacter(ActionEvent event) {
+        editor.textProperty().addListener((observable, oldValue, newValue) -> {
+            int count = newValue.length();
+            System.out.println(count + " Characters");
+        });
     }
 }
